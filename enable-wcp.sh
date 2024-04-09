b3458d3c9921663cdb47c39362de3e992b5b88c5
papivot <47264956+papivot@users.noreply.github.com>
2024-04-08 22:41:07 -0400

    reorganzise and update files for nsx

--- enable-wcp.sh
+++ enable-wcp.sh
@@ -8,10 +8,26 @@ VCENTER_VERSION=8
 VCENTER_HOSTNAME=192.168.100.50
 VCENTER_USERNAME=administrator@vsphere.local
 VCENTER_PASSWORD='VMware1!'
+DEPLOYMENT_TYPE='VDS'
+export DNS_SERVER='192.168.100.1'
+export NTP_SERVER='time.vmware.com'
+export DNS_SEARCHDOMAIN='env1.lab.test'
 
+export MGMT_STARTING_IP='192.168.100.60'
+export MGMT_GATEWAY_CIDR='192.168.100.1/24'
+
+export AVI_CLOUD='domain-c99'
 export AVI_HOSTNAME=192.168.100.58
 export AVI_USERNAME=admin
 export AVI_PASSWORD='VMware1!'
+
+export NSX_EDGE_CLUSTER_ID='408bc83a-e05a-4da5-85cf-aeb4625981c4'
+export NSX_T0_GATEWAY='16110da9-eb11-4c0d-af33-437c9dc7ed3b'
+export NSX_DVS_PORTGROUP='mgmt-vds01'
+#### TO add these
+export NSX_INGRESS_CIDR='10.220.3.16/24'
+export NSX_EGRESS_CIDR='10.220.30.80/24'
+export NSX_NAMESPACE_NETWORK='10.244.0.0/20'
 
 K8S_SUP_CLUSTER=Supervisor-Cluster
 K8S_CONTENT_LIBRARY=utkg
@@ -34,18 +50,27 @@ EOF
 }
 
 rm -f /tmp/temp_*.*
+if [ ${DEPLOYMENT_TYPE} == "VDS" ]
+then
+        cp sample-vds-80.json cluster.json
+else
+        cp sample-nsx-80.json cluster.json
+fi
 
 ################################################
 # Get NSXALB CA CERT
 ###############################################
-echo "Getting NSX ALB CA Certificate for  ${AVI_HOSTNAME} ..."
-openssl s_client -showcerts -connect ${AVI_HOSTNAME}:443  </dev/null 2>/dev/null|sed -ne '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' > /tmp/temp_avi-ca.cert
-if [ ! -s /tmp/temp_avi-ca.cert ]
+if [ ${DEPLOYMENT_TYPE} == "VDS" ]
 then
-    echo "Error: Could not connect to the NSX ALB endpoint. Please validate!!"
-    exit 1
+        echo "Getting NSX ALB CA Certificate for  ${AVI_HOSTNAME} ..."
+        openssl s_client -showcerts -connect ${AVI_HOSTNAME}:443  </dev/null 2>/dev/null|sed -ne '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' > /tmp/temp_avi-ca.cert
+        if [ ! -s /tmp/temp_avi-ca.cert ]
+        then
+                echo "Error: Could not connect to the NSX ALB endpoint. Please validate!!"
+                exit 1
+        fi
+        export AVI_CACERT=$(jq -sR . /tmp/temp_avi-ca.cert)
 fi
-export AVI_CACERT=$(jq -sR . /tmp/temp_avi-ca.cert)
 
 ################################################
 # Login to VCenter and get Session ID
@@ -101,9 +126,7 @@ if [[ ${VCENTER_VERSION} -eq 7 ]] ; then
         	echo "Error: Could not fetch content library - ${K8S_CONTENT_LIBRARY} . Please validate!!"
         	exit 1
 	fi
-	cp VcenterNamespaceManagementClustersInfo-70.json VcenterNamespaceManagementClustersInfo.json 
-else
-	cp VcenterNamespaceManagementClustersInfo-80.json VcenterNamespaceManagementClustersInfo.json 
+	cp sample-vds-70.json cluster.json 
 fi
 
 ################################################
@@ -142,21 +165,47 @@ then
         echo "Error: Could not fetch portgroup - ${K8S_MGMT_PORTGROUP} . Please validate!!"
         exit 1
 fi
-if [ -z "${TKGWorkload0Network}" ]
+
+################################################
+# Get compatiable VDS switch from vCenter
+###############################################
+if [ ${DEPLOYMENT_TYPE} == "NSX" ]
 then
-        echo "Error: Could not fetch portgroup - ${K8S_WKD0_PORTGROUP} . Please validate!!"
-        exit 1
+        echo "Searching for NSX compatible VDS switch ..."
+        response=$(curl -ks --write-out "%{http_code}" -X POST  -H "${HEADER_SESSIONID}" https://${VCENTER_HOSTNAME}/api/vcenter/namespace-management/networks/nsx/distributed-switches?action=check_compatibility --output /tmp/temp_vds.json)
+        if [[ "${response}" -ne 200 ]] ; then
+                echo "Error: Could not fetch VDS details. Please validate!!"
+                exit 1
+        fi
+        export NSX_DVS=$(jq -r --arg NSX_DVS_PORTGROUP "$NSX_DVS_PORTGROUP" '.[]| select((.compatible==true) and .name == $NSX_DVS_PORTGROUP)|.distributed_switch' /tmp/temp_vds.json)
+        if [ -z "${NSX_DVS}" ]
+        then
+                echo "Error: Could not fetch NSX compatible VDS - ${NSX_DVS_PORTGROUP} . Please validate!!"
+                exit 1
+        fi
 fi
-if [ -z "${TKGWorkload1Network}" ]
+
+################################################
+# Get WORKLOAD Netwrok exist for VDS
+###############################################
+if [ ${DEPLOYMENT_TYPE} == "VDS" ]
 then
-        echo "Error: Could not fetch portgroup - ${K8S_WKD0_PORTGROUP} . Please validate!!"
-        exit 1
+        if [ -z "${TKGWorkload0Network}" ]
+        then
+                echo "Error: Could not fetch portgroup - ${K8S_WKD0_PORTGROUP} . Please validate!!"
+                exit 1
+        fi
+        if [ -z "${TKGWorkload1Network}" ]
+        then
+                echo "Error: Could not fetch portgroup - ${K8S_WKD1_PORTGROUP} . Please validate!!"
+                exit 1
+        fi
 fi
 
 ################################################
 # Get cluster details from vCenter
 ###############################################
-envsubst < VcenterNamespaceManagementClustersInfo.json > temp_final.json
+envsubst < cluster.json > temp_final.json
 
 echo "Enabling WCP on cluster ${TKGClusterID} ..."
 curl -ks -X POST -H "${HEADER_SESSIONID}" -H "${HEADER_CONTENTTYPE}" -d "@temp_final.json" https://${VCENTER_HOSTNAME}/api/vcenter/namespace-management/clusters/${TKGClusterID}?action=enable
@@ -164,5 +213,5 @@ curl -ks -X POST -H "${HEADER_SESSIONID}" -H "${HEADER_CONTENTTYPE}" -d "@temp_f
 #while configuring, keep checking for status of Supervisor until ready
 
 rm -f /tmp/temp_*.*
-#rm -f temp_final.json
-rm -f VcenterNamespaceManagementClustersInfo.json
+rm -f temp_final.json
+rm -f cluster.json
